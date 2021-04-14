4fedb6e0a6146372e43f92f2dd6bf45343f81aeb
michellemtchai <michelle.chai@mail.utoronto.ca>
2021-04-14 19:23:47 -0400

    add uncommitted files from production setup

--- config/initialize.js
+++ config/initialize.js
@@ -16,11 +16,16 @@ module.exports = (app) => {
     }
 
     // parse request body
-    app.router.use(bodyParser.urlencoded({ extended: false }));
-    app.router.use(bodyParser.json());
+    app.router.use(
+        bodyParser.urlencoded({ extended: false, limit: '50mb' })
+    );
+    app.router.use(bodyParser.json({ limit: '50mb' }));
 
     // make static files in /public availiable
-    app.router.use('/assets', app.express.static('public'));
+    app.router.use(
+        process.env.APP_PUBLIC_URL,
+        app.express.static('public')
+    );
 
     // set view engine as ejs
     app.router.set('view engine', 'ejs');
